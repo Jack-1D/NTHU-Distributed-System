b1f1381ca2a96b172b06989215f8f3ccae15fa80
Jack-1D <timjackduncan@gmail.com>
2024-04-16 15:03:41 +0000

    finish homework

--- modules/video/stream/stream.go
+++ modules/video/stream/stream.go
@@ -39,12 +39,24 @@ func (s *stream) HandleVideoCreated(ctx context.Context, req *pb.HandleVideoCrea
 
 		// [Kafka TODO]
 		// [Describe] Transcode video if get message with scale != 0, you can handle error occurance like above primitive.ObjectIDFromHex(req.GetId()).
+		err := s.handleVideoWithVariant(ctx, id, variant, req.Url)
+		if err != nil {
+			return nil, &saramakit.HandlerError{Retry: true, Err: err}
+		}
 
 		return &emptypb.Empty{}, nil
 	}
 
 	// [Kafka TODO]
 	// [Describe] Fanout create events to each variant [1080, 720, 480, 320], you can handle error occurance like above primitive.ObjectIDFromHex(req.GetId()).
+	scaleList := []int{1080, 720, 480, 320}
+	for _, scaleNum := range scaleList {
+		req.Scale = int32(scaleNum)
+		err = s.produceVideoCreatedWithScaleEvent(req)
+		if err != nil {
+			return nil, &saramakit.HandlerError{Retry: true, Err: err}
+		}
+	}
 
 	return &emptypb.Empty{}, nil
 }
@@ -72,6 +84,10 @@ func (s *stream) produceVideoCreatedWithScaleEvent(req *pb.HandleVideoCreatedReq
 
 	// [Kafka TODO]
 	// [Describe] Send message to kafka
+	err = s.producer.SendMessages(msgs)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
